b559cdd9daaee6f2946690d05e20fa37211d7fc3
char-kyeopta <charisse.oblefias01@gmail.com>
2022-08-09 12:30:00 +0800

    Add project files.

---
+++ RadiusOfCircle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class RadiusofCircle
+{
+	static void Main (string [] args)
+	{
+		Console.Write ("Enter the radius of the circle: ");
+		int r = Int32.Parse (Console.ReadLine());
+		
+		Console.WriteLine ("The perimeter of the circle is {0:0.0000} ", 2 * Math.PI * r);
+		Console.WriteLine ("The area of the circle is {0:0.0000} ", Math.PI * r * r);
+		Console.ReadKey();
+	}
+}
